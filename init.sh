de280210433cb3cbcdc14dfc4306f05ac31e6d84
Sonder1738 <mc_laren_93@hotmail.com>
2025-05-18 17:53:40 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ SSH_PASSWORD="password"
 sudo apt-get update -y
 sudo apt-get install -y sshpass
 ssh-keygen -t rsa -b 4096 -f "/home/ansible/.ssh/id_rsa" -N ""
-
+chmod 764 dynamic_inventory.py
 export ANSIBLE_HOST_KEY_CHECKING=False
 sshpass -p "$SSH_PASSWORD" ansible-playbook -i dynamic_inventory.py setupKeys.yml \
   -u "$ANSIBLE_USER" --ask-pass --extra-vars "ansible_ssh_pass=$SSH_PASSWORD"
